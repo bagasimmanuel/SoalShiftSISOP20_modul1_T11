e7f77b55f6decb5dbb453fb3dc261bc137154f3b
kecoa queen <55182873+RizaruKecoak@users.noreply.github.com>
2020-02-21 14:57:59 +0700

    update all

--- Shift/soal3/soal3c.sh
+++ Shift/soal3/soal3c.sh
@@ -8,16 +8,13 @@ tes=`ls | grep "pdkt_kusuma_" | cut -d "_" -f 3 | sort -n | tail -1`
 arr=""
 for ((i=1;i<=tes;i=i+1))
 do
-abc="`cat wget.log | grep "Location" | head -$i | tail -1 | cut -d " " -f 2`"
-dupkah=`echo -e $arr | awk -v loc=$abc 'BEGIN{dupkahlagi=1} {if(loc==0) dupkahlagi=0} END' {printf "%d", dupkahlagi}`
-echo $dupkah
+abc=`cat wget.log | grep "Location" | head -$i | tail -1 | cut -d " " -f 2`
+dupkah=`echo -e $arr | awk -v loc=$abc 'BEGIN{dupkahlagi=0} {if(loc==$0) dupkahlagi=1} END {printf "%d", dupkahlagi}'`
 
-if [ $dupkah==0 ]
+if [[ $dupkah == 0 ]]
 then
-
-arr=`$arr$log\n`
+arr="$arr$abc\n"
 mv pdkt_kusuma_$i kenangan/kenangan_$i
-
 else
 mv pdkt_kusuma_$i duplicate/duplicate_$i
 fi
